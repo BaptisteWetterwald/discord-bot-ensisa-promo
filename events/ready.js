d2f5dcbae08a80ee857065c8c8f40a1f6489cce1
BaptisteWetterwald <baptiste.wetterwald@gmail.com>
2023-09-09 14:30:51 +0000

    Update cache at start-up

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,13 @@ const { fetchMenuGeneral } = require('../commands/ensisa/menu');
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+
+		await client.guilds.cache.forEach(guild => {
+			guild.channels.fetch();
+			guild.members.fetch();
+		});
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		
 		client.user.setPresence({
